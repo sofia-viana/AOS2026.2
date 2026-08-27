e7c8e230bfdb502ab0a82fbb7c769c93f57ee034
sofia-viana <sofiavianax9@gmail.com>
2026-08-27 22:43:55 +0000

    subi pro vercel

--- express-01/src/index.js
+++ express-01/src/index.js
@@ -1,1 +1,9 @@
-    console.log("Ola turma");
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Olá Mundo!');
+});
+
+// Importante para o Vercel
+module.exports = app; 
